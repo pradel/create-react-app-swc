3d230b5306255e45fdd42909cb0481a980ab354d
Léo Pradel <pradel.leo@gmail.com>
2020-10-09 18:06:58 +0200

    feat: first version of craco-swc (#1)

---
+++ packages/craco-swc/src/index.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const fs = require("fs");
+const { loaderByName, removeLoaders, addAfterLoader } = require("@craco/craco");
+
+module.exports = {
+  /**
+   * To process the js/ts files we replace the babel-loader with the swc-loader
+   */
+  overrideWebpackConfig: ({
+    webpackConfig,
+    pluginOptions,
+    context: { paths },
+  }) => {
+    const useTypeScript = fs.existsSync(paths.appTsConfig);
+    const appSwcConfig = path.resolve(paths.appPath, ".swcrc");
+    const useSwcConfig = fs.existsSync(appSwcConfig);
+
+    // add swc-loader
+    addAfterLoader(webpackConfig, loaderByName("babel-loader"), {
+      test: /\.(js|mjs|jsx|ts|tsx)$/,
+      include: paths.appSrc,
+      loader: require.resolve("swc-loader"),
+      options:
+        pluginOptions && pluginOptions.swcLoaderOptions
+          ? pluginOptions.swcLoaderOptions
+          : useSwcConfig
+          ? // If there is an .swcrc file at the root of the project we pass undefined
+            // That way swc will use the .swcrc config
+            undefined
+          : {
+              jsc: {
+                externalHelpers: true,
+                target: "es2015",
+                parser: {
+                  // If user is using typescript we need to use the typescript parser
+                  syntax: useTypeScript ? "typescript" : "ecmascript",
+                  jsx: true,
+                },
+              },
+            },
+    });
+
+    // remove the babel loaders
+    removeLoaders(webpackConfig, loaderByName("babel-loader"));
+
+    return webpackConfig;
+  },
+
+  /**
+   * To process the js/ts files we replace the babel-loader with the swc jest loader
+   */
+  overrideJestConfig: ({
+    jestConfig,
+    pluginOptions,
+    context: { env, paths, resolve, rootDir },
+  }) => {
+    // Replace babel transform with swc
+    const key = Object.keys(jestConfig.transform)[0];
+    // TODO find a way to pass options directly to the plugin without having to use a .swcrc
+    jestConfig.transform[key] = [require.resolve("@swc/jest")];
+
+    return jestConfig;
+  },
+};
