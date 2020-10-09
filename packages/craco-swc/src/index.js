b4521d25fd5746fc7c65db71faf6c8284e8b5ba0
pradel <pradel.leo@gmail.com>
2020-10-09 13:18:57 +0200

    first try

--- packages/craco-swc/src/index.js
+++ packages/craco-swc/src/index.js
@@ -1,1 +1,37 @@
-// TODO
+const fs = require("fs");
+const { loaderByName, removeLoaders, addAfterLoader } = require("@craco/craco");
+
+module.exports = {
+  /**
+   * To process the js/ts files we replace the babel-loader with the swc-loader
+   */
+  overrideWebpackConfig: ({
+    webpackConfig,
+    cracoConfig,
+    pluginOptions,
+    context: { env, paths },
+  }) => {
+    const useTypeScript = fs.existsSync(paths.appTsConfig);
+
+    // add swc-loader
+    addAfterLoader(webpackConfig, loaderByName("babel-loader"), {
+      test: /\.(js|mjs|jsx|ts|tsx)$/,
+      include: paths.appSrc,
+      loader: require.resolve("swc-loader"),
+      options: {
+        jsc: {
+          target: "es2015",
+          parser: {
+            syntax: useTypeScript ? "typescript" : "ecmascript",
+            jsx: true,
+          },
+        },
+      },
+    });
+
+    // remove the babel loaders
+    removeLoaders(webpackConfig, loaderByName("babel-loader"));
+
+    return webpackConfig;
+  },
+};
